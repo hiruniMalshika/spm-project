dc974dd12225771ab1b59665219dcd8e7d0b70b4
Hiruni <malshikahiruni99@gmail.com>
2021-10-01 18:29:46 +0530

    make it better3

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ mongoose.connect(URI, {
 })
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
+    app.use(express.static('SPM_explores_CAFE_frontend/build'))
     app.get('*', (req, res) =>{
         res.sendFile(path.join(__dirname, 'SPM_explores_CAFE_frontend', 'build', 'index.html'))
     })
